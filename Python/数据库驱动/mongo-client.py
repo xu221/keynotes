78238f17e19ac35d82315d0312a0a519ccd45f8c
xu221 <xuzelong8@126.com>
2022-01-14 10:30:29 +0800

    新增mongodb驱动

---
+++ Python/数据库驱动/mongo-client.py
@@ -0,0 +1,77 @@
+from pymongo import MongoClient
+from pymongo import WriteConcern
+from pymongo import InsertOne, DeleteOne, ReplaceOne, UpdateOne, DeleteMany
+from pymongo.errors import BulkWriteError
+import time, json
+import bson.timestamp
+
+
+def func1():
+    """
+    查询文档函数
+    """
+    client = MongoClient('mongodb://127.0.0.1:27017/')  # 建立连接
+    collection = client['blogdb']['posts']              # 选择集合
+    for i in collection.find():                         # 遍历集合
+        print(i)
+    time.sleep(2)
+
+def func2():
+    """
+    批量插入文档函数
+    """
+    client = MongoClient('mongodb://127.0.0.1:27017/')  # 建立连接
+    collection = client['blogdb'].get_collection('posts', write_concern=WriteConcern(w=1, j=True, wtimeout=1)) # 选择集合
+        # write_concern控制何时调用getLastError()
+        # write_concern=1：mongod在写入内存之后，返回响应
+        # write_concern=1 & journal:true：mongod在写入内存、journal日志之后，返回响应
+        # write_concern=2：在集群模式生效，2时表示只有secondary从primary完成复制之后，返回响应
+    try:
+        insertData =[InsertOne({'title': i}) for i in range(4)] # 插入文档
+        otherData = [
+                    DeleteMany({}),                             # Remove all documents.
+                    InsertOne({'_id': 1}),
+                    InsertOne({'_id': 2}),
+                    InsertOne({'_id': 3}),
+                    UpdateOne({'_id': 1}, {'$set': {'foo': 'bar'}}),
+                    UpdateOne({'_id': 4}, {'$inc': {'j': 1}}, upsert=True),
+                    ReplaceOne({'j': 1}, {'j': 2}),
+                    DeleteOne({'_id': 2})
+        ]
+        collection.bulk_write(otherData + insertData, ordered=True) 
+    except BulkWriteError as bwe:
+        print(bwe.details)
+
+def func3():
+    """
+    比对两个字典
+    """
+
+def func4(connection_url):
+    """
+    计算库内所有文档记录数
+    """
+    client = MongoClient(connection_url)
+    db_dictionary = {}
+    for db in (x for x in client.list_database_names() if x not in ['admin', 'config']):
+        collention_dictionary = {}
+        for collection in client[db].list_collection_names():
+            collention_dictionary[collection] = client[db][collection].estimated_document_count()
+        db_dictionary[db] = collention_dictionary
+    print(json.dumps(db_dictionary, indent=4))
+
+def func5():
+    """
+    查询近一个小时内活跃的数据库
+    """
+    client = MongoClient('mongodb://127.0.0.1:27017/')  # 建立连接
+    c = client['local']['oplog']                        # 选择oplog集合
+    result = {} 
+    pas_timestamp = round(time.time()) - 86400
+    # 正则匹配：过滤config、admin、空字符开头的namespace，并过滤大于指定时间的日志
+    for i in c.rs.find({"$and":[{"ns": {"$regex": r"^(?!^config)(?!^admin)(?!^$)"}},{"ts": {"$gt" : bson.timestamp.Timestamp(pas_timestamp,1) }}]}):
+        result[i["ns"]]=i["wall"].strftime("%Y-%m-%d %H:%M:%S")
+    print(json.dumps(result, indent=4))
+    
+if __name__ == "__main__":
+    func4("mongodb://x21:x21@127.0.0.1:27017/?authSource=admin")
