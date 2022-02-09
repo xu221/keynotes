03d0d36ca5dcec4634a0fe8d9e02f82b90e09d9c
xu221 <xuzelong8@126.com>
2022-02-09 15:37:21 +0800

    修改LINUX目录

---
+++ Linux/sysinfo.sh
@@ -0,0 +1,25 @@
+# 查看系统内核信息
+uname -a
+
+# 查看系统版本
+cat /etc/redhat-release
+
+# 查看系统内核版本
+cat /proc/version
+
+# 查看当前用户环境变量
+env
+
+cat /proc/cpuinfo
+
+# 查看有几个逻辑cpu, 包括cpu型号
+cat /proc/cpuinfo | grep name | cut -f2 -d: | uniq -c
+
+# 查看有几颗cpu,每颗分别是几核
+cat /proc/cpuinfo | grep physical | uniq -c
+
+# 查看当前CPU运行在32bit还是64bit模式下, 如果是运行在32bit下也不代表CPU不支持64bit
+getconf LONG_BIT
+
+# 结果大于0, 说明支持64bit计算. lm指long mode, 支持lm则是64bit
+cat /proc/cpuinfo | grep flags | grep ' lm ' | wc -l
