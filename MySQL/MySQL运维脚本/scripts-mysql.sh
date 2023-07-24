5a356bdca81ad6667fed6d1df4b5c9d806d511ac
xu221 <48054938+xu221@users.noreply.github.com>
2023-07-24 15:54:17 +0800

    新增MySQL慢慢删除大量数据

--- MySQL/MySQL运维脚本/scripts-mysql.sh
+++ MySQL/MySQL运维脚本/scripts-mysql.sh
@@ -54,3 +54,53 @@ mysqldump -u'' -p'' -h'xx' --set-gtid-purged=OFF --single-transaction --no-creat
 mysqldump -u'' -p'' -h'xx' --set-gtid-purged=OFF --single-transaction --skip-add-drop-table --hex-blob --complete-insert db_00 > db.sql
 mysqldump -u'' -p'' -h'xx' --set-gtid-purged=OFF --single-transaction --skip-add-drop-table --hex-blob --complete-insert db_00 tablename --where='id="775"' > filter_by_id.sql
 # </>
+
+# <分批删除数据，可开多进程并发>
+#!/bin/bash
+
+# 数据库连接信息
+DB_HOST=""
+DB_USER=""
+DB_PASS=""
+DB_NAME="dd"
+
+# 设置目标表名称
+TABLE_NAME="tt"
+
+# 设置每批删除的记录数
+BATCH_SIZE=1000
+
+# 设置删除条件，这里使用 created_at 作为示例
+DELETE_CONDITION='WHERE created_at < "2022-10-01 00:00:00" AND  created_at >= "2022-09-01 00:00:00"'
+
+# 设置每隔多少次循环检查一次是否还有需要删除的记录
+CHECK_INTERVAL=600
+
+# 初始化循环计数器
+counter=0
+
+while true; do
+    # 删除当前批次的数据
+    mysql -h $DB_HOST -u $DB_USER -p$DB_PASS -D $DB_NAME -e "DELETE FROM $TABLE_NAME $DELETE_CONDITION LIMIT $BATCH_SIZE"
+
+    # 增加循环计数器
+    ((counter++))
+
+    # 检查是否需要检查是否还有需要删除的记录
+    if [ $((counter % CHECK_INTERVAL)) -eq 0 ]; then
+        # 获取需要删除的记录总数
+        TOTAL_RECORDS=$(mysql -h $DB_HOST -u $DB_USER -p$DB_PASS -D $DB_NAME -se "SELECT COUNT(*) FROM $TABLE_NAME $DELETE_CONDITION")
+
+        # 检查是否还有需要删除的记录
+        if [ "$TOTAL_RECORDS" -eq "0" ]; then
+            break
+        fi
+        echo "继续跑，还有 " $TOTAL_RECORDS " 行数据。"
+    fi
+
+    # 每删除一批数据后，暂停1秒钟
+    sleep 1
+done
+
+echo "Data deletion completed."
+# </>
